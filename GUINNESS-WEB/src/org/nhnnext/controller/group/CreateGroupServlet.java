f9f4426971e5ad622f0bb9bff5b37fe73e62f62d
probit <probit@10.73.43.229>
2015-03-19 18:53:41 +0900

    [추가] 그룹 생성

---
+++ GUINNESS-WEB/src/org/nhnnext/controller/group/CreateGroupServlet.java
@@ -0,0 +1,62 @@
+package org.nhnnext.controller.group;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.nhnnext.guinness.dao.GroupDAO;
+import org.nhnnext.guinness.model.Group;
+
+@WebServlet("/group/create")
+public class CreateGroupServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String groupId, groupName, groupCaptainUserId, createDate = null;
+		int isPublic = 0;
+		
+		groupName = (String)req.getParameter("groupName");
+		
+		System.out.println(req.getParameter("isPublic"));
+
+		if(req.getParameter("isPublic").equals("on")) {
+			isPublic = 1;
+		}
+		
+		// 그룹 아이디 생성
+		
+		groupId="abcdf";
+		
+		// 그룹 캡틴 유저 아이디
+		
+		//groupCaptainUserId = (String) req.getSession().getAttribute("userId");
+		groupCaptainUserId="test@guinness.org";
+		
+		System.out.println(groupId);
+		System.out.println(groupName);
+		System.out.println(groupCaptainUserId);
+		System.out.println(createDate);		
+		System.out.println(isPublic);
+
+		Group group = new Group(groupId, groupName, groupCaptainUserId, createDate, isPublic);
+		
+		GroupDAO groupDao = new GroupDAO();
+		try {
+			groupDao.createGroup(group);
+		} catch (SQLException e) {
+		}
+		
+		resp.sendRedirect("/groups.jsp");
+		System.out.println("그룹 생성을 성공하였습니다.");
+	
+	}
+	
+	
+}
--- GUINNESS-WEB/src/org/nhnnext/guinness/model/Group.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/model/Group.java
@@ -4,16 +4,40 @@ public class Group {
 	private String groupId;
 	private String groupName;
 	private String groupCaptainUserId;
-	private boolean isPublic;
+	private int isPublic;
+	private String createDate;
 	// createDate
 	
-	public Group(String groupId, String groupName, String groupCaptainUserId,
-			boolean isPublic) {
+	public Group(String groupId, String groupName, String groupCaptainUserId, String createDate,
+			int isPublic) {
 		this.groupId = groupId;
 		this.groupName = groupName;
 		this.groupCaptainUserId = groupCaptainUserId;
+		this.createDate=createDate;
 		this.isPublic = isPublic;
 	}
+
+	public String getGroupId() {
+		return groupId;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public String getGroupCaptainUserId() {
+		return groupCaptainUserId;
+	}
+
+	public int isPublic() {
+		return isPublic;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+	
+	
 	
 	
 }
