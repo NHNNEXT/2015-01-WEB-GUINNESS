84a0c29eeab0f727f1e0ab09687d19cdaa63a28c
Youngbin Jang <jyb0823@gmail.com>
2015-03-20 15:58:45 +0900

    [추가] note.jsp 일지작성 모달

--- GUINNESS-WEB/src/org/nhnnext/guinness/controller/users/CreateNoteServlet.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/controller/users/CreateNoteServlet.java
@@ -3,6 +3,7 @@ package org.nhnnext.guinness.controller.users;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -21,19 +22,28 @@ public class CreateNoteServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
+		req.setCharacterEncoding("utf-8");
+		
 		String userId = req.getParameter("userId");		
 		String groupId = req.getParameter("groupId");
 		String noteText = req.getParameter("noteText");
 		String targetDate = req.getParameter("targetDate");
 		
-		
+		groupId = "abcde";
 		userId = "test@guinness.org";
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DATE, Integer.parseInt(targetDate));
+
+		targetDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(cal.getTime());
+		
 		Note note = new Note(noteText, targetDate, userId, groupId);
 		
 		NoteDAO noteDAO = new NoteDAO();
 		
 		try {
 			noteDAO.createNote(note);
+			resp.sendRedirect("/notes.jsp");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
