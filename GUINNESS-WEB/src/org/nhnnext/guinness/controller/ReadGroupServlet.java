218b49a957ddf6d459f91e1b58a33a371f1d7387
probit <probit@10.73.43.229>
2015-03-23 11:04:53 +0900

    [추가] 그룹리스트 보기

--- GUINNESS-WEB/src/org/nhnnext/guinness/controller/ReadGroupServlet.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/controller/ReadGroupServlet.java
@@ -2,6 +2,7 @@ package org.nhnnext.guinness.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.channels.SeekableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.nhnnext.guinness.common.*;
 import org.nhnnext.guinness.model.Group;
@@ -24,11 +26,13 @@ public class ReadGroupServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 			
-		// 유저아이디를 받아온다
-//		String userId = (String) req.getSession().getAttribute(
-//				SessionKey.SESSION_USERID);
-		String userId = "test@guinness.org";
-
+		HttpSession session = req.getSession();
+		String userId = (String)session.getAttribute(SessionKey.SESSION_USERID);
+		
+		if (userId == null) {
+			resp.sendRedirect("/");
+			return;
+		}
 		// DAO를 이용해 그룹유저맵에서 유저가 속한 그룹의 아이디를 받아온다.
 		GroupDAO groupDao = new GroupDAO();
 		List<String> list = new ArrayList<String>();
@@ -37,9 +41,6 @@ public class ReadGroupServlet extends HttpServlet {
 
 		// 받아온 그룹아이디 출력 테스트
 		createJsonFile(list, resp);
-		
-//		resp.sendRedirect("/groups.jsp");
-		
 	}
 
 	public void createJsonFile(List<String> list, HttpServletResponse resp) {
