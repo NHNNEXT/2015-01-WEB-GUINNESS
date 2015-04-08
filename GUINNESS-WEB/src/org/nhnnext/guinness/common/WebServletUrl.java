5e1de603fbddc74c8c43f655c898d74e99a1aade
Ginam Kim <mocca002@naver.com>
2015-04-08 11:33:24 +0900

    [수정] 노트리스트를 불러오는 readNoteList 매핑 문제 해결

--- GUINNESS-WEB/src/org/nhnnext/guinness/common/WebServletUrl.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/common/WebServletUrl.java
@@ -7,7 +7,7 @@ public class WebServletUrl {
 	public static final String GROUP_ADD_MEMBER = "/group/add/member";
 
 	public static final String NOTELIST = "/g/*";
-	public static final String NOTELIST_READ = "/notelist/read/";
+	public static final String NOTELIST_READ = "/notelist/read";
 	public static final String NOTELIST_CHECK = "/notelist/check";
 	public static final String NOTELIST_UPDATE = "/notelist/update";
 	public static final String NOTE_CREATE = "/note/create";
