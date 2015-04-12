0de597befa7fdf08be2a7d3a3e624a7aa26ce5f8
sungcheon <sungcheon.lee@outlook.com>
2015-04-12 13:39:56 +0900

    mockito 적용 및, ServletRequestUtilTest 생성.

---
+++ GUINNESS-WEB/test/org/nhnnext/guinness/util/ServletRequestUtilTest.java
@@ -0,0 +1,29 @@
+package org.nhnnext.guinness.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Test;
+
+public class ServletRequestUtilTest {
+
+	@Test
+	public void existedUserIdFromSession() throws IOException {
+		HttpServletRequest req = mock(HttpServletRequest.class);
+		HttpServletResponse resp = mock(HttpServletResponse.class);
+		HttpSession session = mock(HttpSession.class);
+		
+		when(req.getSession()).thenReturn(session);
+		when(session.getAttribute("sessionUserId")).thenReturn("testUserId");
+		
+		Boolean bool = ServletRequestUtil.existedUserIdFromSession(req, resp);
+		assertEquals(true, bool);
+	}
+
+}
