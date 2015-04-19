87c95decacacb974c9df33f130404e4cd0519e8c
Ginam Kim <mocca002@naver.com>
2015-04-19 10:01:28 +0900

    userDaoTest

--- GUINNESS-WEB/test/org/nhnnext/guinness/model/UserDaoTest.java
+++ GUINNESS-WEB/test/org/nhnnext/guinness/model/UserDaoTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nhnnext.guinness.exception.AlreadyExistedUserIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,5 +21,12 @@ public class UserDaoTest {
 	public void NotExistCreateUserTest() throws ClassNotFoundException {
 		System.out.println(userDao.readUser("test@guinness.org").toString());
 		assertNotNull(userDao.readUser("test@guinness.org"));
+	}
+	
+	@Test
+	public void CRUDTest() throws ClassNotFoundException, AlreadyExistedUserIdException {
+		userDao.createUser(new User("daoTest@guinness.com","Name","password"));
+		User user = userDao.readUser("daoTest@guinness.com");
+		assertNotNull(user);
 	}
 }
