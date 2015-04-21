19346ae7fc7bfed4f0d60c26a5bafd8f5ba27b76
hwiba <sungcheon.lee@outlook.com>
2015-04-21 17:22:14 +0900

    [삭제] spring 적용으로 불피요해진 class들 제거.

--- GUINNESS-WEB/src/org/nhnnext/guinness/util/ObjectMapper.java
+++
@@ -1,8 +0,0 @@
-package org.nhnnext.guinness.util;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface ObjectMapper<T> {
-	T returnObject(ResultSet rs) throws SQLException;
-}
