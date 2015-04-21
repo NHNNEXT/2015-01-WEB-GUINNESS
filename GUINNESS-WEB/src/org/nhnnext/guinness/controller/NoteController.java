1b02c4458d85b312bf326a855eaddf3c5140d2cf
최성원 <lavie.choi@gmail.com>
2015-04-21 21:05:09 +0900

    [수정] Map Data 제거, HttpServletRequest -> WebRequest로 대체

--- GUINNESS-WEB/src/org/nhnnext/guinness/controller/NoteController.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/controller/NoteController.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.joda.time.DateTime;
@@ -24,6 +21,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -31,7 +29,7 @@ public class NoteController {
 	private static final Logger logger = LoggerFactory.getLogger(NoteController.class);
 	@Autowired
 	private NoteDao noteDao;
-	
+
 	@Autowired
 	private GroupDao groupDao;
 
@@ -47,53 +45,54 @@ public class NoteController {
 		}
 		return "notes";
 	}
-	
+
 	@RequestMapping("/notelist/read")
-	protected ModelAndView readNoteList(HttpServletRequest req) throws IOException {
-		Map<String, String> paramsList = ServletRequestUtil.getRequestParameters(req, "groupId", "targetDate");
-		DateTime targetDate = new DateTime(paramsList.get("targetDate")).plusDays(1).minusSeconds(1);
+	protected ModelAndView readNoteList(WebRequest req) throws IOException {
+		String groupId = req.getParameter("groupId");
+		DateTime targetDate = new DateTime(req.getParameter("targetDate")).plusDays(1).minusSeconds(1);
 		// 임시 : 캘린더가 만들어지기 전까지 임시로 20년 범위로 가져오기.
 		// TODO 추후에는 targetDate에 해당하는 하루치 노트들만 불러올 것.
 		DateTime endDate = targetDate.minusYears(10);
-		targetDate=targetDate.plusYears(10);
+		targetDate = targetDate.plusYears(10);
 		// 임시 : 여기까지.
 		List<Note> noteList = null;
 		try {
-			noteList = noteDao.readNoteList(paramsList.get("groupId"), endDate.toString(), targetDate.toString());
+			noteList = noteDao.readNoteList(groupId, endDate.toString(), targetDate.toString());
 		} catch (Exception e) {
 			logger.error("Exception", e);
 			return new ModelAndView("exception");
 		}
 		return new ModelAndView("jsonView").addObject("jsonData", noteList);
 	}
-	
+
 	@RequestMapping("/note/read")
-	protected ModelAndView show(HttpServletRequest req) throws IOException {
-		Map<String, String> paramsList = ServletRequestUtil.getRequestParameters(req, "noteId");
+	protected ModelAndView show(WebRequest req) throws IOException {
+		String noteId = req.getParameter("noteId");
 		Note note = null;
 		try {
-			note = noteDao.readNote(paramsList.get("noteId"));
+			note = noteDao.readNote(noteId);
 		} catch (MakingObjectListFromJdbcException e) {
 			logger.error("Exception", e);
 			return new ModelAndView("exception");
 		}
 		return new ModelAndView("jsonView").addObject("jsonData", note);
 	}
-	
+
 	@RequestMapping(value = "/note/create", method = RequestMethod.POST)
-	protected String create(HttpSession session, HttpServletRequest req) throws ServletException, IOException {
+	protected String create(WebRequest req, HttpSession session) throws IOException {
 		if (!ServletRequestUtil.existedUserIdFromSession(session)) {
 			return "redirect:/";
 		}
 		String sessionUserId = ServletRequestUtil.getUserIdFromSession(session);
-		Map<String, String> paramsList = ServletRequestUtil.getRequestParameters(req, "groupId", "noteText",
-				"targetDate");
-		String targetDate = paramsList.get("targetDate") + " "
+		String groupId = req.getParameter("groupId");
+		String noteText = req.getParameter("noteText");
+		String targetDate = req.getParameter("targetDate") + " "
 				+ new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
-		if (paramsList.get("noteText").equals("")) {
-			return "redirect:/g/"+paramsList.get("groupId");
+		;
+		if (noteText.equals("")) {
+			return "redirect:/g/" + groupId;
 		}
-		noteDao.createNote(new Note(paramsList.get("noteText"), targetDate, sessionUserId, paramsList.get("groupId")));
-		return "redirect:/g/"+paramsList.get("groupId");
+		noteDao.createNote(new Note(noteText, targetDate, sessionUserId, groupId));
+		return "redirect:/g/" + groupId;
 	}
 }
