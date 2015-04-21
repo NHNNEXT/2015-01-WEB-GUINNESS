843fe90a7b2a9befc4feaf8d740d47cf54f929ad
Youngbin Jang <jyb0823@gmail.com>
2015-04-21 15:14:13 +0900

    [수정] CommentDao RowMapper를 lambda expression사용

--- GUINNESS-WEB/src/org/nhnnext/guinness/model/CommentDao.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/model/CommentDao.java
@@ -1,11 +1,8 @@
 package org.nhnnext.guinness.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.nhnnext.guinness.exception.MakingObjectListFromJdbcException;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 public class CommentDao extends JdbcDaoSupport {
@@ -16,12 +13,15 @@ public class CommentDao extends JdbcDaoSupport {
 	}
 
 	public List<Comment> readCommentListByNoteId(String noteId) throws MakingObjectListFromJdbcException,
-			ClassNotFoundException {
+	ClassNotFoundException {
 		String sql = "select * from COMMENTS, USERS where COMMENTS.userId = USERS.userId AND noteId = ?";
-		return getJdbcTemplate().query(sql, new RowMapper<Comment>() {
-			public Comment mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Comment(rs.getString("commentText"), rs.getString("commentType"), rs.getString("createDate"), rs.getString("userId"), rs.getString("noteId"), rs.getString("userName"));
-			}
-		}, noteId);
+		
+		return getJdbcTemplate().query(sql, (rs, rowNum) -> new Comment(
+				rs.getString("commentText"), 
+				rs.getString("commentType"), 
+				rs.getString("createDate"), 
+				rs.getString("userId"), 
+				rs.getString("noteId"), 
+				rs.getString("userName")), noteId);
 	}
 }
