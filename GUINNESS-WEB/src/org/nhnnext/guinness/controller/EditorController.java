8ab69fd00852e44b2a69aff90e334622428d5329
Youngbin Jang <jyb0823@gmail.com>
2015-05-14 11:26:31 +0900

    [수정] 노트 생성, 노트 수정 controller method 분리  - mapping url 수정 : /note/editor -> /editor/{noteId}

--- GUINNESS-WEB/src/org/nhnnext/guinness/controller/EditorController.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/controller/EditorController.java
@@ -4,9 +4,12 @@ import javax.servlet.http.HttpSession;
 
 import org.nhnnext.guinness.dao.GroupDao;
 import org.nhnnext.guinness.dao.NoteDao;
+import org.nhnnext.guinness.model.Group;
+import org.nhnnext.guinness.model.Note;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
@@ -19,20 +22,34 @@ public class EditorController {
 	private GroupDao groupDao;
 	@Autowired
 	private NoteDao noteDao;
-	
-	@RequestMapping(value="/note/editor", method=RequestMethod.GET)
-	private String Editor (WebRequest req, HttpSession session, Model model)  {
-		String groupId = req.getParameter("groupId");		
+
+	@RequestMapping(value = "/note/editor", method = RequestMethod.GET)
+	private String Editor(WebRequest req, HttpSession session, Model model) {
+		String groupId = req.getParameter("groupId");
 		String noteId = req.getParameter("noteId");
-		if(noteId != null){
+		if (noteId != null) {
 			model.addAttribute("noteId", noteId);
 			model.addAttribute("noteText", noteDao.readNote(noteId).getNoteText());
 		}
-		if(groupId != null){		
+		if (groupId != null) {
 			String groupName = groupDao.readGroup(groupId).getGroupName();
 			model.addAttribute("groupId", groupId);
 			model.addAttribute("groupName", new Gson().toJson(groupName));
-		}		
+		}
+		return "editor";
+	}
+
+	@RequestMapping(value = "/editor/{noteId}", method = RequestMethod.GET)
+	private String Editor(@PathVariable String noteId, Model model) {
+		Note note = noteDao.readNote(noteId);
+		Group group = groupDao.readGroup(note.getGroup().getGroupId());
+
+		model.addAttribute("noteId", noteId);
+		model.addAttribute("noteText", note.getNoteText());
+
+		model.addAttribute("groupId", group.getGroupId());
+		model.addAttribute("groupName", new Gson().toJson(group.getGroupName()));
+
 		return "editor";
 	}
 }
