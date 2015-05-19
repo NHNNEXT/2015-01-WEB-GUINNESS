b464eba0c2b5efce74f70402c82b9edf6cc0af1b
probitanima11 <probitanima11@gmail.com>
2015-05-19 14:20:05 +0900

    [수정] 회원가입 서버사이드 유효성검사 적용

--- GUINNESS-WEB/src/org/nhnnext/guinness/controller/UserController.java
+++ GUINNESS-WEB/src/org/nhnnext/guinness/controller/UserController.java
@@ -1,26 +1,25 @@
 package org.nhnnext.guinness.controller;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import javax.validation.ConstraintViolation;
+import javax.validation.Valid;
 
 import org.nhnnext.guinness.exception.AlreadyExistedUserIdException;
 import org.nhnnext.guinness.exception.FailedLoginException;
 import org.nhnnext.guinness.exception.NotExistedUserIdException;
 import org.nhnnext.guinness.exception.SendMailException;
 import org.nhnnext.guinness.exception.UserUpdateException;
-import org.nhnnext.guinness.model.SessionUser;
 import org.nhnnext.guinness.model.User;
 import org.nhnnext.guinness.service.UserService;
 import org.nhnnext.guinness.util.JsonResult;
-import org.nhnnext.guinness.util.MyValidatorFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,27 +29,31 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Resource
 	private UserService userService;
 
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	protected String create(Model model, User user) throws AlreadyExistedUserIdException, SendMailException {
-		// 유효성 체크
-		if (!extractViolationMessage(model, user)) {
-			return "index";
-		}
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	protected String create(@Valid User user, BindingResult result, Model model) throws AlreadyExistedUserIdException, SendMailException {
+		if(result.hasErrors()) {
+            List<ObjectError> list = result.getAllErrors();
+            for (ObjectError e : list) {
+            	String element = e.getCodes()[0].split("\\.")[2];
+            	model.addAttribute(element+"_message", result.getFieldError(element).getDefaultMessage());
+            	logger.debug("field: {}, message: {}", element, result.getFieldError(element).getDefaultMessage());
+            }
+            return "index";
+        }
 		userService.join(user);
 		return "sendEmail";
 	}
 
-	@RequestMapping("/confirm/{keyAddress}")
+	@RequestMapping(value = "/user/confirm/{keyAddress}")
 	protected String confirm(@PathVariable String keyAddress, HttpSession session) {
-		SessionUser sessionUser = userService.confirm(keyAddress).createSessionUser();
-		saveUserInfoInSession(session, sessionUser);
+		User user = userService.confirm(keyAddress);
+		saveUserInfoInSession(session, user);
 		return "redirect:/";
 	}
 	
@@ -58,8 +61,8 @@ public class UserController {
 	protected @ResponseBody boolean login(WebRequest req, HttpSession session) throws FailedLoginException {
 		String userId = req.getParameter("userId");
 		String userPassword = req.getParameter("userPassword");
-		SessionUser sessionUser = (userService.login(userId, userPassword)).createSessionUser();
-		saveUserInfoInSession(session, sessionUser);
+		User user = userService.login(userId, userPassword);
+		saveUserInfoInSession(session, user);
 		return true;
 	}
 	
@@ -69,23 +72,23 @@ public class UserController {
 		return "redirect:/";
 	}
 
-	@RequestMapping("/form")
+	@RequestMapping(value = "/user/form")
 	protected String updateForm(Model model) {
 		model.addAttribute("user", new User());
 		return "updateUser";
 	}
 	
-	@RequestMapping(value = "/update/check", method = RequestMethod.POST)
+	@RequestMapping(value = "/user/update/check", method = RequestMethod.POST)
 	protected @ResponseBody JsonResult updateUserCheck(HttpSession session, WebRequest req){
 		String userPassword = req.getParameter("password");
-		String userId = ((SessionUser)session.getAttribute("sessionUser")).getUserId();
+		String userId = (String) session.getAttribute("sessionUserId");
 		boolean result = userService.checkUpdatePassword(userId, userPassword);
 		if(!result)
 			return new JsonResult().setSuccess(result).setMessage("비밀번호를 확인해주세요");
 		return new JsonResult().setSuccess(result); 
 	}
 
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
+	@RequestMapping(value = "/user/update", method = RequestMethod.POST)
 	protected String updateUser(WebRequest req, HttpSession session, Model model, User user,
 			@RequestParam("profileImage") MultipartFile profileImage) throws UserUpdateException {
 		String userAgainPassword = req.getParameter("userAgainPassword");
@@ -94,39 +97,26 @@ public class UserController {
 
 		String rootPath = session.getServletContext().getRealPath("/");
 		userService.update(user, model, rootPath, profileImage);
-		saveUserInfoInSession(session, user.createSessionUser());
-		return "redirect:/groups/form";
+
+		saveUserInfoInSession(session, user);
+		return "redirect:/groups";
 	}
 
-	@RequestMapping("/findPasswordForm")
+	@RequestMapping("/user/findPasswordForm")
 	protected String findPasswordForm(HttpSession session) {
 		return "findPassword";
 	}
 	
-	@RequestMapping(value = "/findPassword", method = RequestMethod.POST)
+	@RequestMapping(value = "/user/findPassword", method = RequestMethod.POST)
 	protected String findPassword(@RequestParam("userId") String userId, Model model) throws NotExistedUserIdException, SendMailException {
 		userService.initPassword(userId);
 		model.addAttribute("message", "임시 비밀번호를 이메일로 보내드렸습니다.");
 		return "sendEmail";
 	}
 	
-	private void saveUserInfoInSession(HttpSession session, SessionUser sessionUser) {
-		session.setAttribute("sessionUser", sessionUser);
-	}
-
-	private boolean extractViolationMessage(Model model, User user) {
-		Set<ConstraintViolation<User>> constraintViolations = MyValidatorFactory.createValidator().validate(user);
-		if (!constraintViolations.isEmpty()) {
-			String signValidErrorMessage = "";
-			Iterator<ConstraintViolation<User>> violations = constraintViolations.iterator();
-			while (violations.hasNext()) {
-				ConstraintViolation<User> each = violations.next();
-				signValidErrorMessage = signValidErrorMessage + "<br />" + each.getMessage();
-			}
-			model.addAttribute("signValidErrorMessage", signValidErrorMessage);
-			logger.debug("violation error");
-			return false;
-		}
-		return true;
+	private void saveUserInfoInSession(HttpSession session, User user) {
+		session.setAttribute("sessionUserId", user.getUserId());
+		session.setAttribute("sessionUserName", user.getUserName());
+		session.setAttribute("sessionUserImage", user.getUserImage());
 	}
 }
