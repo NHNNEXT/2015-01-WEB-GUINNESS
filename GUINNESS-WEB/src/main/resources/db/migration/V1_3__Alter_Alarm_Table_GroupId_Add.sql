30da08a76d9c854b831a3b32607052ee0ddd9869
최성원 <lavie.choi@gmail.com>
2015-05-19 10:06:35 +0900

    [수정] flayway 1_3 version invitedGroupId 오타 수

--- GUINNESS-WEB/src/main/resources/db/migration/V1_3__Alter_Alarm_Table_GroupId_Add.sql
+++ GUINNESS-WEB/src/main/resources/db/migration/V1_3__Alter_Alarm_Table_GroupId_Add.sql
@@ -1,7 +1,7 @@
 ALTER TABLE ALARMS ADD invitedGroupId char(5) NOT NULL;
 ALTER TABLE ALARMS MODIFY invitedGroupId char(5) NOT NULL after noteId;
 SET foreign_key_checks = 0;
-ALTER TABLE ALARMS ADD FOREIGN KEY (AlarmGroupId) REFERENCES GROUPS(groupId) ON DELETE CASCADE;
+ALTER TABLE ALARMS ADD FOREIGN KEY (invitedGroupId) REFERENCES GROUPS(groupId) ON DELETE CASCADE;
 SET foreign_key_checks = 1;
 
 /* 그룹초대 알림을 위한 가상노트, 가상코멘트 */
