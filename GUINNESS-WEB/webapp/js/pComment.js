90830cafeaff8c10d44250289acb5845578b4807
hwiba <sungcheon.lee@outlook.com>
2015-05-22 14:10:47 +0900

    getSelection을 통한 pComment 팝업 버튼의 위치 수정.

--- GUINNESS-WEB/webapp/js/pComment.js
+++ GUINNESS-WEB/webapp/js/pComment.js
@@ -1,93 +1,107 @@
 var mousePosition = {};
 mousePosition.downPoint = {
-	x : 0,
-	y : 0
+    x: 0,
+    y: 0
 };
 mousePosition.upPoint = {
-	x : 0,
-	y : 0
+    x: 0,
+    y: 0
 };
 
-pComment = {
-	pCommentText : null,
-	selectedText : null,
-	sameSenCount : 0,
-	sameSenIndex : 0,
-	userId : null,
-	pId : null,
-	noteId : null
+var pComment = {
+    pCommentText: null,
+    selectedText: null,
+    sameSenCount: 0,
+    sameSenIndex: 0,
+    userId: null,
+    pId: null,
+    noteId: null
 };
 
 function selectText() {
-	var select = "";
-	if (document.getSelection) {
-		select = document.getSelection();
-	} else if (document.selection) {
-		select = document.selection.createRange().text;
-	}
-	var selectedText = select.toString();
-	if (selectedText.length > 0) {
-		return selectedText;
-	}
-	return false;
+    var select = "";
+    if (document.getSelection) {
+        select = document.getSelection();
+    } else if (document.selection) {
+        select = document.selection.createRange().text;
+    }
+    var selectedText = select.toString();
+    if (selectedText.length > 0) {
+        return selectedText;
+    }
+    return false;
 }
 
 function createPopupPCommentBtn() {
-	var templatePopupBtn = document.querySelector("#popupCommentBtnTemplate").text;
-	document.body.insertAdjacentHTML("afterend", templatePopupBtn);
+    var templatePopupBtn = document.querySelector("#popupCommentBtnTemplate").text;
+    document.body.insertAdjacentHTML("afterend", templatePopupBtn);
 
-	var popupCommentBtn = document.querySelector(".popupCommentBtn");
-	popupCommentBtn.addEventListener('click', function(e) {
-		e.target.style.display = "none";
-	}, false);
+    var popupCommentBtn = document.querySelector(".popupCommentBtn");
+    popupCommentBtn.addEventListener('click', function (e) {
+        e.target.style.display = "none";
+        // 코멘트 입력 창을 나타나게 하고
+        // 이 코멘트 입력창에서 pComment 객체를 가져다가 서버와 통신하게 할 것.
+    }, false);
 }
 
 function setPopupPCommentBtn() {
-	var elNoteText = document.body.querySelector(".note-content");
+    var elNoteText = document.body.querySelector(".note-content");
 
-	elNoteText.addEventListener('mousedown', function(e) {
-		mousePosition.downPoint.x = e.pageX;
-		mousePosition.downPoint.y = e.pageY;
-	}, false);
+    //elNoteText.addEventListener('mousedown', function (e) {
+    //    mousePosition.downPoint.x = e.clientX;
+    //    mousePosition.downPoint.y = e.clientY;
+    //}, false);
 
-	elNoteText
-			.addEventListener(
-					'mouseup',
-					function(e) {
-						mousePosition.upPoint.x = e.pageX;
-						mousePosition.upPoint.y = e.pageY;
+    elNoteText.addEventListener('mouseup', function (e) {
+        //mousePosition.upPoint.x = e.clientX;
+        //mousePosition.upPoint.y = e.clientY;
 
-						var selectedText = selectText();
-						var selectedElClass = getSelection().getRangeAt(0).commonAncestorContainer;
-						var selectedElClassName = selectedElClass.parentElement !== null ? selectedElClass.parentElement.className
-								: noteText;
+        //var left = mousePosition.upPoint.x < mousePosition.downPoint.x ? mousePosition.upPoint.x : mousePosition.downPoint.x;
+        //left += Math.abs(mousePosition.upPoint.x - mousePosition.downPoint.x);
+        //var top = mousePosition.upPoint.y;
 
-						var elPopupBtn = document
-								.querySelector(".popupCommentBtn");
-						var left = mousePosition.upPoint.x < mousePosition.downPoint.x ? mousePosition.upPoint.x
-								: mousePosition.downPoint.x;
-						left += Math.abs(mousePosition.upPoint.x
-								- mousePosition.downPoint.x);
-						var top = mousePosition.upPoint.y;
+        var elPopupBtn = document.querySelector(".popupCommentBtn");
+        var selectedText = selectText();
+        var selectedRect = window.getSelection().getRangeAt(0).getBoundingClientRect();
+        var selectedElClass = window.getSelection().getRangeAt(0).commonAncestorContainer;
+        if (selectedText && selectedElClass.className !== "note-content") {
+            elPopupBtn.style.top = (selectedRect.top-30) + "px";
+            elPopupBtn.style.left = ((selectedRect.left+selectedRect.right)/2)-31 + "px";
+            elPopupBtn.style.display = "block";
+            pComment.selectedText = selectedText;
+            pComment.pId = getPid(selectedElClass);
+            setSameSentence(pComment, selectedText, window.getSelection());
+            pComment.userId = "";
+            pComment.noteId = 0;
+        } else {
+            elPopupBtn.style.display = "none";
+        }
+    }, false);
 
-						if (selectedText) {
-							//&& "note-content" !== selectedElClassName) {
-							elPopupBtn.style.top = top + "px";
-							elPopupBtn.style.left = left + "px";
-							elPopupBtn.style.display = "block";
-							pComment.selectedText = selectedText;
-							pComment.noteId = 0;
-							pComment.pId = 0;
-							pComment.sameSenCount = 1;
-							pComment.sameSenIndex = 1;
-							pComment.userId = "";
-						} else {
-							elPopupBtn.style.display = "none";
-						}
-					}, false);
+    // <span class="highlighted"></span> 를 document.body.innerText.search("") 를
+    // 사용해 찾고, 각 뒤, 앞 순서로 삽입한다.
+    // 이후, 해당 클래스에 색을 없게 주고, 코멘트에 hover 시, 색이 있게 한다.
+}
 
-	// <span class="highlighted"></span> 를 document.body.innerText.search("") 를
-	// 사용해 찾고, 각 뒤, 앞 순서로 삽입한다.
-	// 이후, 해당 클래스에 색을 없게 주고, 코멘트에 hover 시, 색이 있게 한다.
-	// .getRangeAt(0).commonAncestorContainer.parentElement
+function getPid (selectedElClass) {
+    while (selectedElClass.tagName !== "P" && selectedElClass.tagName !== "PRE") {
+        selectedElClass = selectedElClass.parentNode;
+    }
+    return selectedElClass.id;
+}
+
+function setSameSentence (pComment, selectedText, selection) {
+    var pId = pComment.pId;
+    var pText = document.body.querySelector("#"+pId).innerText;
+
+    var sameTexts = new Array();
+    var sameText = pText.indexOf(selectedText);
+    while(sameText !== -1){
+        sameTexts.push(sameText);
+        sameText = pText.indexOf(selectedText, sameText + selectedText.length);
+    }
+    pComment.sameSenCount = sameTexts.length;
+    pComment.sameSenIndex = 0;
+
+    var t = selection;
 }
